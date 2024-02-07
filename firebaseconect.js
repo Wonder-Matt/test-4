a8dfb5eb53a078c7f372865e6cd95a5c899ff9f3
Wonder-Matt <159161107+Wonder-Matt@users.noreply.github.com>
2024-02-07 13:19:04 +0100

    Update firebaseconect.js

--- firebaseconect.js
+++ firebaseconect.js
@@ -5,13 +5,14 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey:,
-  authDomain:,
-  projectId:,
-  storageBucket:,
-  messagingSenderId:,
-  appId:,
-  measurementId:
+  apiKey: "AIzaSyAXfA2ZIeCoB8nHZ7F_ceP_gxCTb2vnFPw",
+  authDomain: "login-production-f1404.firebaseapp.com",
+  databaseURL: "https://login-production-f1404-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "login-production-f1404",
+  storageBucket: "login-production-f1404.appspot.com",
+  messagingSenderId: "113980090667",
+  appId: "1:113980090667:web:ef3f392973b9242380bd6e",
+  measurementId: "G-KDHKEQ6F3C"
 };
 
 const app = initializeApp(firebaseConfig);
